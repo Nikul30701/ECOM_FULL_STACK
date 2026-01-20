fd0925aaa966974a9c58336ba8cdc15f5ffcee5b
Nikul <p.nikul6403@gmail.com>
2026-01-20 21:06:02 +0530

    created a models of Ecom in Django and half serializer

---
+++ Backend/serializers.py
@@ -0,0 +1,76 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff']
+        read_only_fields = ['id']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name')
+
+    class Meta:
+        models = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'category_name', 
+                    'stock', 'image', 'is_active', 'is_in_stock', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'product_count', 'created_at']
+        read_only_fields = ['id', 'created_at']
+    
+    def get_product_count(self, obj):
+        return obj.products.filter(is_active=True).count()
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.IntegerField(write_only=True)
+    subtotal = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity', 'subtotal']
+        read_only_fields = ['id', 'subtotal']
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(id=value, is_active=True).exists():
+            raise serializers.ValidationError("Product not found")
+        return value
+    
+    def validate(self, attrs):
+        product = Product.objects.get(id=attrs['product_id'])
+        if attrs['qauantity'] > product.stock:
+            raise serializers.ValidationError({"quantity": f"Only {product.stock} items available in stock."})
+        return attrs
+        
